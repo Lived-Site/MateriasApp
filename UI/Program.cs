94132a2a6227dd7164a97f891b3efcae7b4c7491
Francisco Menchaca Giorello <franmenchaca2004@gmail.com>
2026-07-24 15:58:39 -0300

    Feature: Aplicacion de escriotrio

--- UI/Program.cs
+++ UI/Program.cs
@@ -1,5 +1,8 @@
 using Repository;
 using UI.Components;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Hosting.Server.Features;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,5 +26,27 @@ app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+app.Lifetime.ApplicationStarted.Register(() =>
+{
+    try
+    {
+        var server = app.Services.GetRequiredService<IServer>();
+        var addresses = server.Features.Get<IServerAddressesFeature>()?.Addresses;
 
+        var url = addresses?.FirstOrDefault();
+
+        if (!string.IsNullOrEmpty(url))
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = url,
+                UseShellExecute = true
+            });
+        }
+    }
+    catch
+    {
+        // Si no se puede abrir el navegador, la aplicación sigue funcionando.
+    }
+});
 app.Run();
